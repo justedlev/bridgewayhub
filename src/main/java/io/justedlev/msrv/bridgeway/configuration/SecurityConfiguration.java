d1642b2ec6b92acb0354628d3bd95b5447382a7a
Justedlev <edyardlyk@gmail.com>
2024-09-20 22:38:44 +0300

    feat(security): update logout handler
    use exist `ServerLogoutSuccessHandler`

--- src/main/java/io/justedlev/msrv/bridgeway/configuration/SecurityConfiguration.java
+++ src/main/java/io/justedlev/msrv/bridgeway/configuration/SecurityConfiguration.java
@@ -1,6 +1,5 @@
 package io.justedlev.msrv.bridgeway.configuration;
 
-import io.justedlev.msrv.bridgeway.component.KeycloakLogoutHandler;
 import io.justedlev.msrv.bridgeway.configuration.properties.SecurityProperties;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -10,33 +9,26 @@ import org.springframework.lang.NonNull;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.client.oidc.web.server.logout.OidcClientInitiatedServerLogoutSuccessHandler;
+import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
 import org.springframework.security.web.server.SecurityWebFilterChain;
-import reactor.core.publisher.Mono;
 
 @Slf4j
 @Configuration
 @EnableWebFluxSecurity
 @RequiredArgsConstructor
 public class SecurityConfiguration {
-    private static final String LOGOUT = "/logout";
-
-    private final KeycloakLogoutHandler keycloakLogoutHandler;
     private final SecurityProperties securityProperties;
 
     @Bean
-    public SecurityWebFilterChain securityFilterChain(@NonNull ServerHttpSecurity httpSecurity) {
+    public SecurityWebFilterChain securityFilterChain(@NonNull ServerHttpSecurity httpSecurity,
+                                                      ReactiveClientRegistrationRepository rcrr) {
         return httpSecurity
                 .csrf(ServerHttpSecurity.CsrfSpec::disable)
                 .oauth2Login(Customizer.withDefaults())
                 .oauth2ResourceServer(spec -> spec.jwt(Customizer.withDefaults()))
-                .logout(logoutHandler -> logoutHandler
-                        .logoutUrl(LOGOUT)
-                        .logoutHandler(keycloakLogoutHandler)
-                        .logoutSuccessHandler(
-                                (exchange, authentication) -> Mono.just(authentication)
-                                        .doOnNext(s -> log.debug("{}", s))
-                                        .then()
-                        )
+                .logout(logoutSpec -> logoutSpec
+                        .logoutSuccessHandler(new OidcClientInitiatedServerLogoutSuccessHandler(rcrr))
                 )
                 .authorizeExchange(spec -> spec
                         .pathMatchers(securityProperties.getWhiteListArray()).permitAll()
