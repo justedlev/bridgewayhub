567b23a54a3420b25da861f0a9fed9ee7f10c910
Justedlev <edyardlyk@gmail.com>
2024-09-23 21:03:29 +0300

    refactor: remove property classes

--- src/main/java/io/justedlev/msrv/bridgeway/BridgeWayHubApplication.java
+++ src/main/java/io/justedlev/msrv/bridgeway/BridgeWayHubApplication.java
@@ -1,21 +1,12 @@
 package io.justedlev.msrv.bridgeway;
 
-import io.justedlev.msrv.bridgeway.configuration.properties.KeycloakClientProperties;
-import io.justedlev.msrv.bridgeway.configuration.properties.KeycloakProperties;
-import io.justedlev.msrv.bridgeway.configuration.properties.SecurityProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 @RefreshScope
 @EnableDiscoveryClient
-@EnableConfigurationProperties({
-        KeycloakProperties.class,
-        KeycloakClientProperties.class,
-        SecurityProperties.class,
-})
 @SpringBootApplication
 public class BridgeWayHubApplication {
 
