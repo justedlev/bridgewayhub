e0b30d9e36f29edb0b6bc2e8b2c72a3b2b70a3bf
Edward L. <edward.lukashevits@gmail.com>
2025-08-26 15:48:34 +0300

    build(docker): fix healthcheck script
    Signed-off-by: Edward L. <edward.lukashevits@gmail.com>

--- docker/healthcheck.sh
+++ docker/healthcheck.sh
@@ -9,7 +9,8 @@
 
 echo "$0: checking..."
 CONTEXTPATH=${SERVER_SERVLET_CONTEXTPATH:-}
-URL="http://localhost:$SERVER_PORT$CONTEXTPATH/actuator/health"
+PORT=${MANAGEMENT_SERVER_PORT:${SERVER_PORT:8765}}
+URL="http://localhost:${PORT}${CONTEXTPATH}/actuator/health"
 RESPONSE=$(curl -s --fail "$URL")
 STATUS=$(echo "$RESPONSE" | grep -o '"status":"[^"]*"' | cut -d ':' -f2 | tr -d '"')
 echo "$0: status: $STATUS"
