4a398dc5770f956025461eb72033c3713c41ad48
Edward L. <edward.lukashevits@gmail.com>
2025-10-13 20:00:15 +0300

    feat(config): improve oauth2 security configuration
    Signed-off-by: Edward L. <edward.lukashevits@gmail.com>

--- src/main/java/io/justedlev/msrv/bridgeway/configuration/BridgeWaySecurityConfiguration.java
+++ src/main/java/io/justedlev/msrv/bridgeway/configuration/BridgeWaySecurityConfiguration.java
@@ -1,28 +1,27 @@
 package io.justedlev.msrv.bridgeway.configuration;
 
+import io.justedlev.msrv.bridgeway.configuration.properties.BridgeWaySecurityConfigurationProperties;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.security.oauth2.client.ConditionalOnOAuth2ClientRegistrationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.lang.NonNull;
 import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.oauth2.client.AuthorizedClientServiceReactiveOAuth2AuthorizedClientManager;
-import org.springframework.security.oauth2.client.ReactiveOAuth2AuthorizedClientManager;
-import org.springframework.security.oauth2.client.ReactiveOAuth2AuthorizedClientService;
 import org.springframework.security.oauth2.client.oidc.web.server.logout.OidcClientInitiatedServerLogoutSuccessHandler;
 import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Slf4j
 @Configuration
-@EnableWebFluxSecurity
+@EnableConfigurationProperties(BridgeWaySecurityConfigurationProperties.class)
+@ConditionalOnOAuth2ClientRegistrationProperties
 @RequiredArgsConstructor
-public class SecurityConfiguration {
-    private static final String DEFAULT_AUTHORITY_PREFIX = "SCOPE_";
-    private final SecurityProperties properties;
+public class BridgeWaySecurityConfiguration {
+    private final BridgeWaySecurityConfigurationProperties props;
 
     @Bean
     public SecurityWebFilterChain securityFilterChain(@NonNull ServerHttpSecurity httpSecurity,
@@ -35,20 +34,11 @@ public class SecurityConfiguration {
                         .logoutSuccessHandler(new OidcClientInitiatedServerLogoutSuccessHandler(rcrr))
                 )
                 .authorizeExchange(spec -> {
-                    properties.getWhitelist().forEach((k, v) -> spec.pathMatchers(k, v).permitAll());
+                    props.getWhitelist().forEach((k, v) -> spec.pathMatchers(k, v).permitAll());
                     spec.pathMatchers(HttpMethod.GET, "/actuator/prometheus")
-                            .hasAuthority(DEFAULT_AUTHORITY_PREFIX + "prometheus.metrics:ro");
+                            .hasAuthority(props.getScopePrefix() + "prometheus.metrics:ro");
                     spec.anyExchange().authenticated();
                 })
                 .build();
-    }
-
-    @Bean
-    ReactiveOAuth2AuthorizedClientManager authorizedClientServiceReactiveOAuth2AuthorizedClientManager(
-            ReactiveClientRegistrationRepository clientRegistrationRepository,
-            ReactiveOAuth2AuthorizedClientService authorizedClientService
-    ) {
-        return new AuthorizedClientServiceReactiveOAuth2AuthorizedClientManager(
-                clientRegistrationRepository, authorizedClientService);
     }
 }
