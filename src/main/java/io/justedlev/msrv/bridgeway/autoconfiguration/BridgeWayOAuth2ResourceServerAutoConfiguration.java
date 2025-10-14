ce76eaf37b80b61f941dcc0a7ee8759a3db653df
Edward L. <edward.lukashevits@gmail.com>
2025-10-14 17:02:09 +0300

    fix(config): use reactive configuration
    Signed-off-by: Edward L. <edward.lukashevits@gmail.com>

--- src/main/java/io/justedlev/msrv/bridgeway/autoconfiguration/BridgeWayOAuth2ResourceServerAutoConfiguration.java
+++ src/main/java/io/justedlev/msrv/bridgeway/autoconfiguration/BridgeWayOAuth2ResourceServerAutoConfiguration.java
@@ -5,12 +5,12 @@ import lombok.Setter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBooleanProperty;
-import org.springframework.boot.autoconfigure.security.oauth2.resource.servlet.OAuth2ResourceServerAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.reactive.ReactiveOAuth2ResourceServerAutoConfiguration;
 import org.springframework.context.annotation.Import;
 
 @AutoConfiguration
 @ConditionalOnBooleanProperty(prefix = "spring.security.oauth2.resourceserver", name = "enabled", matchIfMissing = true)
-@Import(OAuth2ResourceServerAutoConfiguration.class)
+@Import(ReactiveOAuth2ResourceServerAutoConfiguration.class)
 public class BridgeWayOAuth2ResourceServerAutoConfiguration {
     @Getter
     @Setter(onMethod_ = @Value("${spring.security.oauth2.resourceserver.enabled:true}"))
